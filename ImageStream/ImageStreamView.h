4fad171123141b1c661d2ed17ea0ea0f263ca80b
Michal Kreft <yomajkel@gmail.com>
2015-07-11 15:55:13 +0200

    Initial Commit

---
+++ ImageStream/ImageStreamView.h
@@ -0,0 +1,13 @@
+//
+//  ImageStreamView.h
+//  ImageStream
+//
+//  Created by Kreft, Michal on 11.07.15.
+//  Copyright © 2015 yomajkel. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ImageStreamView : ScreenSaverView
+
+@end
---
+++ ImageStream/ImageStreamView.m
@@ -0,0 +1,52 @@
+//
+//  ImageStreamView.m
+//  ImageStream
+//
+//  Created by Kreft, Michal on 11.07.15.
+//  Copyright © 2015 yomajkel. All rights reserved.
+//
+
+#import "ImageStreamView.h"
+
+@implementation ImageStreamView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
